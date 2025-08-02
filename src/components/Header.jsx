c4b46edc920e314d6d00a6d101238901d0864ad9
Diogo Oike Kanefuku <diogooikekanefukujapan@gmail.com>
2025-08-02 06:03:00 -0300

    rebase older version

---
+++ src/components/Header.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <header className='header' id='menu'>
+      <nav className='navbar navbar-expand-lg navbar-dark' id='nav-style'>
+        <button
+          id='nav-toggler'
+          className='navbar-toggler ms-auto' // Adicione ms-auto aqui
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarNav'
+          aria-controls='navbarNav'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id='navbarNav'>
+          <ul className='navbar-nav ms-auto' id='nav-list'>
+            <li className='nav-item'>
+              <button
+                className='nav-link'
+                onClick={() => scrollToSection('projects')}
+              >
+                PROJECTS
+              </button>
+            </li>
+            <li className='nav-item'>
+              <button
+                className='nav-link'
+                onClick={() => scrollToSection('EB')}
+              >
+                ACADEMIC
+              </button>
+            </li>
+            <li className='nav-item'>
+              <button
+                className='nav-link'
+                onClick={() => scrollToSection('skills')}
+              >
+                SKILLS
+              </button>
+            </li>
+            <li className='nav-item'>
+              <button
+                className='nav-link'
+                onClick={() => scrollToSection('contact')}
+              >
+                CONTACT
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className='my-info'>
+        <div className='my-role'>
+          <h1>DIOGO OIKE</h1>
+          <div className='my-functions'>
+            <div className='main-role'>
+              <span>DATA ENGINEER</span>{' '}
+              <div className='function-icons'>
+                <i className='fa-brands fa-python'></i>
+                <i className='fa-solid fa-database'></i>
+                <i class='fa-solid fa-chart-simple'></i>
+              </div>
+            </div>
+            <div className='secondary-role'>
+              <span>BACKEND DEVELOPER</span>{' '}
+              <div className='function-icons'>
+                <i class='fa-brands fa-node-js'></i>
+                <i class='fa-solid fa-bolt'></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='about-me'>
+          <p className='about-text'>
+            Developing smart solutions with{' '}
+            <span className='header-styleTech header-stylePY'>Python</span>,{' '}
+            <span className='header-styleTech header-styleJV'>Java</span>, and{' '}
+            <span className='header-styleTech header-styleJS'>JavaScript</span>. Tech enthusiast, always{' '}
+            <span className='styleEnph'>learning</span> and evolving.
+          </p>
+        </div>
+      </div>
+      <button
+        className='fixed-button'
+        onClick={() => scrollToSection('menu')}
+        title='Back to menu'
+      >
+        <i className='fa-solid fa-arrow-up'></i>
+      </button>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,92 @@
+import "./Projects.css";
+import React from "react";
+
+const projectData = [
+  {
+    name: "Coin Viewer",
+    category: "Data Science",
+    className: "data-science",
+    description:
+      "A currency quotation application built with Python and Streamlit.",
+    githubRepository: "https://github.com/Dnaka27/Coin_Viewer",
+    techs: [
+      "fa-brands fa-python",
+      "fa-solid fa-database",
+      "fa-solid fa-chart-simple",
+    ],
+  },
+  {
+    name: "To Do List",
+    category: "Web Development",
+    className: "web-development",
+    description:
+      "An organizational to-do list application using React and JavaScript.",
+    githubRepository: "https://github.com/Dnaka27/ToDoList_basic/",
+    techs: ["fa-brands fa-react", "fa-brands fa-square-js", "fa-brands fa-css"],
+  },
+];
+
+const ProjectContent = ({ project }) => (
+  <div className="project-content">
+    <div className="project-header">
+      <h3>{project.name}</h3>
+      <a
+        href={project.githubRepository}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="project-link"
+        aria-label={`View project ${project.name} on GitHub`}
+      >
+        <i className="fa-brands fa-square-github"></i>
+      </a>
+    </div>
+    <div className="project-techs">Imagem</div>
+    <p className="project-description">{project.description}</p>
+  </div>
+);
+
+const Projects = () => (
+  <section id="projects" className="sectionMain">
+    <h2 className="titleSection">PROJECTS</h2>
+    <div className="accordion" id="projectAccordion">
+      {projectData.map((project, index) => (
+        <div className="accordion-item" key={index}>
+          <h2 className="accordion-header" id={`heading${index}`}>
+            <button
+              className={`accordion-button ${index !== 0 ? "collapsed" : ""}`}
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target={`#collapse${index}`}
+              aria-expanded={index === 0 ? "true" : "false"}
+              aria-controls={`collapse${index}`}
+            >
+              <div className="d-flex align-items-center w-100">
+                <span className="project-name">{project.name}</span>
+                <div className={`project-category project-${project.className}`}>{project.category}</div>
+                <div className="tech-icons">
+                  {project.techs.map((tech, techIndex) => (
+                    <i key={techIndex} className={tech}></i>
+                  ))}
+                </div>
+              </div>
+            </button>
+          </h2>
+          <div
+            id={`collapse${index}`}
+            className={`accordion-collapse collapse ${
+              index === 0 ? "show" : ""
+            }`}
+            aria-labelledby={`heading${index}`}
+            data-bs-parent="#projectAccordion"
+          >
+            <div className="accordion-body">
+              <ProjectContent project={project} />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  </section>
+);
+
+export default Projects;
